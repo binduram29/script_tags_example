0c34ba23da72cb10da25db33e0bb78d12836ae55
bindu <bindu.devi@kloctechnologies.co.in>
2018-05-16 17:04:17 +0530

    home controller done

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ShopifyApp::AuthenticatedController
 	SHOPIFY_API_KEY = '4e1908b4ef0313bf06a08ed291a1bc21'
 	SHOPIFY_SHARED_SECRET = '28b7f7e036321e8e43dda4f0b46ba791'
   def index
-    @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
+        @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     #binding.pry
     @url = 'https://' + ShopifyAPI::Shop.current.domain
     @password = 'Test@1234'
@@ -13,8 +13,7 @@ class HomeController < ShopifyApp::AuthenticatedController
     script_tags = ShopifyAPI::ScriptTag.all
     p "111111111111111111111111111111111"
     p script_tags.inspect 
-    #ShopifyAPI::ScriptTag.create(:event => "onload", :src => "https://s3.ap-south-1.amazonaws.com/bindu-bhashwith/hello.js")
-    binding.pry
+    ShopifyAPI::ScriptTag.create(:event => "onload", :src => "https://s3.ap-south-1.amazonaws.com/bindu-bhashwith/scripttag_test.js")
     @webhooks = ShopifyAPI::Webhook.find(:all)
   end
 end
